430575ebfdaf15f0c114ca3c8723f514afd6e1c0
Saie12 <saieshsasane@gmail.com>
2024-01-20 14:34:23 +0530

    Fix logout functionality..

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -10,7 +10,7 @@ import {
     registerUser, 
     updateAccountDetails, 
     updateUserAvatar, 
-    updateUserCoverImage
+    updateUserCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
