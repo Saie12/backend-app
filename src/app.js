45f131244c1a0bdd09dfa76e8974fc3849404ecf
Saie12 <saieshsasane@gmail.com>
2024-01-23 19:54:45 +0530

    added subscription router

--- src/app.js
+++ src/app.js
@@ -20,7 +20,8 @@ import userRouter from './routes/user.routes.js';
 import commentRouter from "./routes/comment.routes.js";
 import dashboardRouter from "./routes/dashboard.routes.js";
 import healthcheckRouter from "./routes/healthcheck.routes.js";
-import playlistRouter from "./routes/playlist.routes.js"
+import playlistRouter from "./routes/playlist.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
 
 
 // routes declaration
@@ -29,5 +30,6 @@ app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/dashboard", dashboardRouter)
 app.use("/api/v1/healthcheck", healthcheckRouter)
 app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
 
 export { app }
